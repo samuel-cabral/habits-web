7504b05e50f1c6e53fb2672db8b6a1c31c4cc882
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-01-21 12:14:57 -0300

    feat: home page fished

--- src/components/Habit.tsx
+++ src/components/Habit.tsx
@@ -1,11 +1,10 @@
-interface HabitProps {
-  completed: number
-} 
+// interface HabitDayProps {
+//   completed: number
+// } 
 
-export function Habit(props: HabitProps) {
+export function HabitDay() {
   return (
-    <div className="bg-zinc-900 w-10 h-10 text-white rounded m-2 text-center flex item-center justify-center">
-      {props.completed}
+    <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg">
     </div>
   )
 }
