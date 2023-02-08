afb266d4f512fbfeb71634bce18bff01d0f2fb73
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-02-07 23:19:49 -0300

    progresso do habit day personalizado

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -34,7 +34,13 @@ export function SummaryTable(): JSX.Element {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map(date => {
-          return <HabitDay key={date.toString()} />
+          return (
+          <HabitDay
+            key={date.toString()} 
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          )
         })}
 
         {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_, index) => {
