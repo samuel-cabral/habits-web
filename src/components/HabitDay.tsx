39a21dab498b8a86095057bfd5b281cdd81b2e62
Samuel Cabral <samuelcabral.mail@gmail.com>
2023-02-07 21:43:31 -0300

    feat: progress bar of the habit day

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -11,11 +11,11 @@ export function HabitDay() {
       <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
 
       <Popover.Portal>
-        <Popover.Content className="min-w=[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
           <span className="font-semibold text-zinc-400">segunda-feira</span> 
           <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span> 
 
-          <ProgressBar progress={75} />
+          <ProgressBar progress={40} />
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
         </Popover.Content>
